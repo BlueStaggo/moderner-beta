31862e2cfd3915d8fb600e0efcaba9594a041e47
forkiesassds <icanttellyou@memeware.net>
2025-01-12 05:14:48 +0200

    Use tags whenever possible

--- fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/ModernBetaTagProviderBiome.java
+++ fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/ModernBetaTagProviderBiome.java
@@ -659,17 +659,14 @@ public class ModernBetaTagProviderBiome extends FabricTagProvider<Biome> {
             );
 
         getOrCreateTagBuilder(BiomeTags.SPAWNS_COLD_VARIANT_FARM_ANIMALS)
+            .addTag(IS_TAIGA)
             .add(
-                    ModernBetaBiomes.BETA_TAIGA,
-                    ModernBetaBiomes.PE_TAIGA,
-                    ModernBetaBiomes.LATE_BETA_TAIGA,
-                    ModernBetaBiomes.EARLY_RELEASE_TAIGA,
                     ModernBetaBiomes.LATE_BETA_EXTREME_HILLS,
                     ModernBetaBiomes.EARLY_RELEASE_EXTREME_HILLS
             );
 
         getOrCreateTagBuilder(BiomeTags.SPAWNS_WARM_VARIANT_FARM_ANIMALS)
-            .add(ModernBetaBiomes.BETA_SAVANNA)
+            .addTag(IS_SAVANNA)
             .addTag(IS_RAINFOREST);
     }
     

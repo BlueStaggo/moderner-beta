ec5b4a4ef2caead8f52d4b0ebf6a3ceff1e5b9b6
forkiesassds <icanttellyou@memeware.net>
2025-03-26 18:36:19 +0200

    Move client specific init code to be called from client initializer

--- neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/ModernerBetaNeoForgeClient.java
+++ neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/ModernerBetaNeoForgeClient.java
@@ -7,13 +7,15 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLConstructModEvent;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @EventBusSubscriber(modid = ModernerBeta.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModernerBetaNeoForgeClient {
     @SubscribeEvent
-    public static void clientInit(FMLConstructModEvent event) {
+    public static void clientInit(FMLClientSetupEvent event) {
+        ModernerBeta.clientInit();
+
         ModLoadingContext.get().registerExtensionPoint(
                 IConfigScreenFactory.class, () -> (mc, screen) -> AutoConfig.getConfigScreen(ModernBetaConfig.class, screen).get());
     }

75ac4a070823bbe1b0975694ed0d100eccb3d258
forkiesassds <icanttellyou@memeware.net>
2025-05-17 15:24:01 +0300

    Fix beta water colours not working on NeoForge

--- fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/ModernerBetaFabricClient.java
+++ fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/ModernerBetaFabricClient.java
@@ -1,11 +1,28 @@
 package mod.bluestaggo.modernerbeta.fabric;
 
 import mod.bluestaggo.modernerbeta.ModernerBeta;
+import mod.bluestaggo.modernerbeta.client.color.BlockColorSampler;
+import mod.bluestaggo.modernerbeta.fabric.client.resource.ModernBetaFabricColormapResource;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
 
 public class ModernerBetaFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModernerBeta.clientInit();
+
+        ResourceManagerHelper resourceManager = ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES);
+        resourceManager.registerReloadListener(new ModernBetaFabricColormapResource(
+                ModernerBeta.createId("water_colormap"),
+                "textures/colormap/water.png",
+                BlockColorSampler.INSTANCE.colormapWater::setColormap
+        ));
+
+        resourceManager.registerReloadListener(new ModernBetaFabricColormapResource(
+                ModernerBeta.createId("underwater_colormap"),
+                "textures/colormap/underwater.png",
+                BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
+        ));
     }
 }
--- neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/ModernerBetaNeoForgeClient.java
+++ neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/ModernerBetaNeoForgeClient.java
@@ -2,12 +2,15 @@ package mod.bluestaggo.modernerbeta.neoforge;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import mod.bluestaggo.modernerbeta.ModernerBeta;
+import mod.bluestaggo.modernerbeta.client.color.BlockColorSampler;
+import mod.bluestaggo.modernerbeta.client.resource.ModernBetaColormapResource;
 import mod.bluestaggo.modernerbeta.config.ModernBetaConfig;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @EventBusSubscriber(modid = ModernerBeta.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -18,5 +21,18 @@ public class ModernerBetaNeoForgeClient {
 
         ModLoadingContext.get().registerExtensionPoint(
                 IConfigScreenFactory.class, () -> (mc, screen) -> AutoConfig.getConfigScreen(ModernBetaConfig.class, screen).get());
+    }
+
+    @SubscribeEvent
+    public static void addClientReloadListeners(AddClientReloadListenersEvent event) {
+        event.addListener(ModernerBeta.createId("water_colormap"), new ModernBetaColormapResource(
+                "textures/colormap/water.png",
+                BlockColorSampler.INSTANCE.colormapWater::setColormap
+        ));
+
+        event.addListener(ModernerBeta.createId("underwater_colormap"), new ModernBetaColormapResource(
+                "textures/colormap/underwater.png",
+                BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
+        ));
     }
 }

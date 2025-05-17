d5e64774d2f2b84c55c4c42fd07d75d56a5878e1
forkiesassds <icanttellyou@memeware.net>
2025-05-17 15:24:01 +0300

    Fix beta water colours not working on Forge

--- forge/src/main/java/mod/bluestaggo/modernerbeta/forge/ModernerBetaForgeClient.java
+++ forge/src/main/java/mod/bluestaggo/modernerbeta/forge/ModernerBetaForgeClient.java
@@ -2,14 +2,16 @@ package mod.bluestaggo.modernerbeta.forge;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import mod.bluestaggo.modernerbeta.ModernerBeta;
+import mod.bluestaggo.modernerbeta.client.color.BlockColorSampler;
+import mod.bluestaggo.modernerbeta.client.resource.ModernBetaColormapResource;
 import mod.bluestaggo.modernerbeta.config.ModernBetaConfig;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 
 @Mod.EventBusSubscriber(modid = ModernerBeta.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModernerBetaForgeClient {
@@ -19,5 +21,18 @@ public class ModernerBetaForgeClient {
 
         ModLoadingContext.get().registerExtensionPoint(
                 ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> AutoConfig.getConfigScreen(ModernBetaConfig.class, screen).get()));
+    }
+
+    @SubscribeEvent
+    public static void addClientReloadListeners(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(new ModernBetaColormapResource(
+                "textures/colormap/water.png",
+                BlockColorSampler.INSTANCE.colormapWater::setColormap
+        ));
+
+        event.registerReloadListener(new ModernBetaColormapResource(
+                "textures/colormap/underwater.png",
+                BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
+        ));
     }
 }

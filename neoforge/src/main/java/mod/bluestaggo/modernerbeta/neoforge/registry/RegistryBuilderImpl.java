8d461b54927fb57f9f1b2c1810921770d355b89e
forkiesassds <icanttellyou@memeware.net>
2025-05-27 21:55:35 +0300

    Cleanup

--- neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/registry/RegistryBuilderImpl.java
+++ neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/registry/RegistryBuilderImpl.java
@@ -3,12 +3,13 @@ package mod.bluestaggo.modernerbeta.neoforge.registry;
 import mod.bluestaggo.modernerbeta.registry.IRegistryBuilder;
 import net.minecraft.registry.Registry;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
+import net.neoforged.neoforge.registries.RegistryBuilder;
 
 public class RegistryBuilderImpl<T> implements IRegistryBuilder<T> {
     private final NewRegistryEvent event;
-    private final net.neoforged.neoforge.registries.RegistryBuilder<T> registryBuilder;
+    private final RegistryBuilder<T> registryBuilder;
 
-    public RegistryBuilderImpl(NewRegistryEvent event, net.neoforged.neoforge.registries.RegistryBuilder<T> registryBuilder) {
+    public RegistryBuilderImpl(NewRegistryEvent event, RegistryBuilder<T> registryBuilder) {
         this.event = event;
         this.registryBuilder = registryBuilder;
     }
--- neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/registry/RegistryHelperImpl.java
+++ neoforge/src/main/java/mod/bluestaggo/modernerbeta/neoforge/registry/RegistryHelperImpl.java
@@ -6,17 +6,18 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
+import net.neoforged.neoforge.registries.RegistryBuilder;
 
 public record RegistryHelperImpl(NewRegistryEvent event) implements IRegistryHelper {
     @Override
     public <T> IRegistryBuilder<T> createSimple(RegistryKey<Registry<T>> key) {
-        net.neoforged.neoforge.registries.RegistryBuilder<T> registryBuilder = new net.neoforged.neoforge.registries.RegistryBuilder<>(key);
+        RegistryBuilder<T> registryBuilder = new RegistryBuilder<>(key);
         return new RegistryBuilderImpl<>(event, registryBuilder);
     }
 
     @Override
     public <T> IRegistryBuilder<T> createDefaulted(RegistryKey<Registry<T>> key, Identifier defaultKey) {
-        net.neoforged.neoforge.registries.RegistryBuilder<T> registryBuilder = new net.neoforged.neoforge.registries.RegistryBuilder<>(key).defaultKey(defaultKey);
+        RegistryBuilder<T> registryBuilder = new RegistryBuilder<>(key).defaultKey(defaultKey);
         return new RegistryBuilderImpl<>(event, registryBuilder);
     }
 }

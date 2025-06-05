627241d3ea252aabb6986dabf22b96f836674a36
Blue Staggo <owengrenney@gmail.com>
2025-06-05 19:07:05 +0100

    John Bersion Brange

--- forgelike/src/main/java/mod/bluestaggo/modernerbeta/forgelike/ModEventsClient.java
+++ forgelike/src/main/java/mod/bluestaggo/modernerbeta/forgelike/ModEventsClient.java
@@ -13,17 +13,26 @@ import mod.bluestaggo.modernerbeta.registry.IRegistryHandler;
 import mod.bluestaggo.modernerbeta.registry.IRegistryHelper;
 import mod.bluestaggo.modernerbeta.registry.VanillaRegistryHandler;
 import net.minecraft.registry.Registry;
+import net.minecraft.resource.ResourceReloader;
+import net.minecraft.util.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+//? if >=1.21.4 {
 import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
+//?} else if >=1.21.2 {
+/*import net.neoforged.neoforge.client.event.AddReloadListenerEvent;
+*///?} else {
+/*import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
+*///?}
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @EventBusSubscriber(modid = ModernerBeta.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -59,13 +68,31 @@ public class ModEventsClient {
     }
 
     @SubscribeEvent
-    public static void addClientReloadListeners(AddClientReloadListenersEvent event) {
-        event.addListener(ModernerBeta.createId("water_colormap"), new ModernBetaColormapResource(
+    public static void addClientReloadListeners(
+        //? if >=1.21.4 {
+        AddClientReloadListenersEvent event
+         //?} else if >=1.21.2 {
+        /*AddReloadListenerEvent event
+         *///?} else {
+        /*RegisterClientReloadListenersEvent event
+        *///?}
+    ) {
+        BiConsumer<Identifier, ResourceReloader> addListener = (id, resourceReloader) -> {
+            //? if >=1.21.4 {
+            event.addListener(id, resourceReloader);
+             //?} else if >=1.21.2 {
+            /*event.addListener(resourceReloader);
+             *///?} else {
+            /*event.registerReloadListener(resourceReloader);
+            *///?}
+        };
+
+        addListener.accept(ModernerBeta.createId("water_colormap"), new ModernBetaColormapResource(
                 "textures/colormap/water.png",
                 BlockColorSampler.INSTANCE.colormapWater::setColormap
         ));
 
-        event.addListener(ModernerBeta.createId("underwater_colormap"), new ModernBetaColormapResource(
+        addListener.accept(ModernerBeta.createId("underwater_colormap"), new ModernBetaColormapResource(
                 "textures/colormap/underwater.png",
                 BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
         ));

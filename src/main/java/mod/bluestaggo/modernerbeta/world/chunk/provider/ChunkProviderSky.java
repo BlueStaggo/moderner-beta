3d0f31caddfea24ecd1cd8cc695f8a90e16c4279
forkiesassds <icanttellyou@memeware.net>
2025-10-11 00:48:59 +0300

    feat: Make sky noise provider support isles

--- src/main/java/mod/bluestaggo/modernerbeta/world/chunk/provider/ChunkProviderSky.java
+++ src/main/java/mod/bluestaggo/modernerbeta/world/chunk/provider/ChunkProviderSky.java
@@ -8,11 +8,13 @@ import mod.bluestaggo.modernerbeta.util.chunk.ChunkHeightmap;
 import mod.bluestaggo.modernerbeta.util.noise.PerlinOctaveNoise;
 import mod.bluestaggo.modernerbeta.world.biome.ModernBetaBiomeSource;
 import mod.bluestaggo.modernerbeta.world.chunk.ModernBetaChunkGenerator;
+import mod.bluestaggo.modernerbeta.world.chunk.provider.island.IslandShape;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
@@ -21,6 +23,7 @@ import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.noise.NoiseConfig;
 
 import java.util.Random;
+import java.util.function.Function;
 
 public class ChunkProviderSky extends ChunkProviderNoise {
     private final PerlinOctaveNoise minLimitOctaveNoise;
@@ -192,6 +195,8 @@ public class ChunkProviderSky extends ChunkProviderNoise {
         int noiseX = startNoiseX + localNoiseX;
         int noiseZ = startNoiseZ + localNoiseZ;
 
+        double islandOffset = this.getIslandOffset(noiseX, noiseZ);
+
         double coordinateScale = this.noiseScale.coordinate();
         double heightScale = this.noiseScale.height();
 
@@ -260,6 +265,7 @@ public class ChunkProviderSky extends ChunkProviderNoise {
             }
             
             density -= densityOffset;
+            density += islandOffset;
             
             // Sample without post-processing
             heightmapDensity = density;

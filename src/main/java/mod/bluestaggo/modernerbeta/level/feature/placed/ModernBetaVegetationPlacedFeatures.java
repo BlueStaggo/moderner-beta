efb0c9fa535b0d1a39dd34541fd8cabdb2860e6e
forkiesassds <icanttellyou@memeware.net>
2025-11-10 23:10:32 +0200

    chore: Remove weird duplicate import statements IntelliJ randomly snuck in

--- src/main/java/mod/bluestaggo/modernerbeta/level/feature/placed/ModernBetaVegetationPlacedFeatures.java
+++ src/main/java/mod/bluestaggo/modernerbeta/level/feature/placed/ModernBetaVegetationPlacedFeatures.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import mod.bluestaggo.modernerbeta.level.feature.ModernBetaFeatureTags;
 import mod.bluestaggo.modernerbeta.level.feature.configured.ModernBetaVegetationConfiguredFeatures;
 import mod.bluestaggo.modernerbeta.level.feature.placement.*;
-import mod.bluestaggo.modernerbeta.level.feature.placement.*;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.Vec3i;

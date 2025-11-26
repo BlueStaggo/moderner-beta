fea8780e4851cabda4f7f9aa5f8e516340fbea62
forkiesassds <icanttellyou@memeware.net>
2025-11-26 17:39:58 +0200

    chore: Make surface configs hold reference to biome tag, so I don't accidentally remove the tags again

--- fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/ModernBetaSurfaceConfigs.java
+++ fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/ModernBetaSurfaceConfigs.java
@@ -4,6 +4,7 @@ import mod.bluestaggo.modernerbeta.ModernerBeta;
 import mod.bluestaggo.modernerbeta.api.level.chunk.surface.SurfaceBlocks;
 import mod.bluestaggo.modernerbeta.api.level.chunk.surface.SurfaceConfig;
 import mod.bluestaggo.modernerbeta.registry.ModernBetaResourceKeys;
+import mod.bluestaggo.modernerbeta.tags.ModernBetaBiomeTags;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 
@@ -28,24 +29,24 @@ public class ModernBetaSurfaceConfigs {
     public static final ResourceKey<SurfaceConfig> SNOW_STONE = keyOf("snow_stone");
 
     public static void bootstrap(BootstrapContext<SurfaceConfig> context) {
-        context.register(SAND, new SurfaceConfig(SurfaceBlocks.SAND, SurfaceBlocks.SAND, SurfaceBlocks.GRAVEL));
-        context.register(RED_SAND, new SurfaceConfig(SurfaceBlocks.RED_SAND, SurfaceBlocks.RED_SAND, SurfaceBlocks.GRAVEL));
-        context.register(BADLANDS, new SurfaceConfig(SurfaceBlocks.BADLANDS, SurfaceBlocks.RED_SAND, SurfaceBlocks.GRAVEL));
-        context.register(NETHER, new SurfaceConfig(SurfaceBlocks.NETHER, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL));
-        context.register(WARPED_NYLIUM, new SurfaceConfig(SurfaceBlocks.WARPED_NYLIUM, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL));
-        context.register(CRIMSON_NYLIUM, new SurfaceConfig(SurfaceBlocks.CRIMSON_NYLIUM, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL));
-        context.register(BASALT, new SurfaceConfig(SurfaceBlocks.BASALT));
-        context.register(SOUL_SOIL, new SurfaceConfig(SurfaceBlocks.SOUL_SOIL));
-        context.register(END, new SurfaceConfig(SurfaceBlocks.END));
-        context.register(GRASS, new SurfaceConfig(SurfaceBlocks.GRASS));
-        context.register(MUD, new SurfaceConfig(SurfaceBlocks.MUD));
-        context.register(MYCELIUM, new SurfaceConfig(SurfaceBlocks.MYCELIUM));
-        context.register(PODZOL, new SurfaceConfig(SurfaceBlocks.PODZOL));
-        context.register(STONE, new SurfaceConfig(SurfaceBlocks.STONE));
-        context.register(SNOW, new SurfaceConfig(SurfaceBlocks.SNOW));
-        context.register(SNOW_DIRT, new SurfaceConfig(SurfaceBlocks.SNOW_DIRT));
-        context.register(SNOW_PACKED_ICE, new SurfaceConfig(SurfaceBlocks.SNOW_PACKED_ICE));
-        context.register(SNOW_STONE, new SurfaceConfig(SurfaceBlocks.SNOW_STONE));
+        context.register(SAND, new SurfaceConfig(SurfaceBlocks.SAND, SurfaceBlocks.SAND, SurfaceBlocks.GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_SAND));
+        context.register(RED_SAND, new SurfaceConfig(SurfaceBlocks.RED_SAND, SurfaceBlocks.RED_SAND, SurfaceBlocks.GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_RED_SAND));
+        context.register(BADLANDS, new SurfaceConfig(SurfaceBlocks.BADLANDS, SurfaceBlocks.RED_SAND, SurfaceBlocks.GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_BADLANDS));
+        context.register(NETHER, new SurfaceConfig(SurfaceBlocks.NETHER, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_NETHER));
+        context.register(WARPED_NYLIUM, new SurfaceConfig(SurfaceBlocks.WARPED_NYLIUM, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_WARPED_NYLIUM));
+        context.register(CRIMSON_NYLIUM, new SurfaceConfig(SurfaceBlocks.CRIMSON_NYLIUM, SurfaceBlocks.NETHER_SOUL_SAND, SurfaceBlocks.NETHER_GRAVEL, ModernBetaBiomeTags.SURFACE_CONFIG_CRIMSON_NYLIUM));
+        context.register(BASALT, new SurfaceConfig(SurfaceBlocks.BASALT, ModernBetaBiomeTags.SURFACE_CONFIG_BASALT));
+        context.register(SOUL_SOIL, new SurfaceConfig(SurfaceBlocks.SOUL_SOIL, ModernBetaBiomeTags.SURFACE_CONFIG_SOUL_SOIL));
+        context.register(END, new SurfaceConfig(SurfaceBlocks.END, ModernBetaBiomeTags.SURFACE_CONFIG_END));
+        context.register(GRASS, new SurfaceConfig(SurfaceBlocks.GRASS, ModernBetaBiomeTags.SURFACE_CONFIG_GRASS));
+        context.register(MUD, new SurfaceConfig(SurfaceBlocks.MUD, ModernBetaBiomeTags.SURFACE_CONFIG_MUD));
+        context.register(MYCELIUM, new SurfaceConfig(SurfaceBlocks.MYCELIUM, ModernBetaBiomeTags.SURFACE_CONFIG_MYCELIUM));
+        context.register(PODZOL, new SurfaceConfig(SurfaceBlocks.PODZOL, ModernBetaBiomeTags.SURFACE_CONFIG_PODZOL));
+        context.register(STONE, new SurfaceConfig(SurfaceBlocks.STONE, ModernBetaBiomeTags.SURFACE_CONFIG_STONE));
+        context.register(SNOW, new SurfaceConfig(SurfaceBlocks.SNOW, ModernBetaBiomeTags.SURFACE_CONFIG_SNOW));
+        context.register(SNOW_DIRT, new SurfaceConfig(SurfaceBlocks.SNOW_DIRT, ModernBetaBiomeTags.SURFACE_CONFIG_SNOW_DIRT));
+        context.register(SNOW_PACKED_ICE, new SurfaceConfig(SurfaceBlocks.SNOW_PACKED_ICE, ModernBetaBiomeTags.SURFACE_CONFIG_SNOW_PACKED_ICE));
+        context.register(SNOW_STONE, new SurfaceConfig(SurfaceBlocks.SNOW_STONE, ModernBetaBiomeTags.SURFACE_CONFIG_SNOW_STONE));
     }
 
     private static ResourceKey<SurfaceConfig> keyOf(String id) {
--- src/main/java/mod/bluestaggo/modernerbeta/api/level/chunk/surface/SurfaceConfig.java
+++ src/main/java/mod/bluestaggo/modernerbeta/api/level/chunk/surface/SurfaceConfig.java
@@ -12,20 +12,21 @@ import net.minecraft.world.level.biome.Biome;
 
 import java.util.Optional;
 
-public record SurfaceConfig(SurfaceBlocks normal, SurfaceBlocks beachSand, SurfaceBlocks beachGravel) {
+public record SurfaceConfig(SurfaceBlocks normal, SurfaceBlocks beachSand, SurfaceBlocks beachGravel, TagKey<Biome> biomeTag) {
     public static final Codec<SurfaceConfig> CODEC = RecordCodecBuilder.create(
         instance -> instance.group(
             SurfaceBlocks.CODEC.fieldOf("normal").forGetter(SurfaceConfig::normal),
             SurfaceBlocks.CODEC.fieldOf("beachSand").forGetter(SurfaceConfig::beachSand),
-            SurfaceBlocks.CODEC.fieldOf("beachGravel").forGetter(SurfaceConfig::beachGravel)
+            SurfaceBlocks.CODEC.fieldOf("beachGravel").forGetter(SurfaceConfig::beachGravel),
+            TagKey.codec(Registries.BIOME).fieldOf("biomeTag").forGetter(SurfaceConfig::biomeTag)
         ).apply(instance, SurfaceConfig::new)
     );
 
-    public SurfaceConfig(SurfaceBlocks surfaceBlocks) {
-        this(surfaceBlocks, surfaceBlocks, surfaceBlocks);
+    public SurfaceConfig(SurfaceBlocks surfaceBlocks, TagKey<Biome> biomeTag) {
+        this(surfaceBlocks, surfaceBlocks, surfaceBlocks, biomeTag);
     }
     
-    public static final SurfaceConfig DEFAULT = new SurfaceConfig(SurfaceBlocks.GRASS, SurfaceBlocks.SAND, SurfaceBlocks.GRAVEL);
+    public static final SurfaceConfig DEFAULT = new SurfaceConfig(SurfaceBlocks.GRASS, SurfaceBlocks.SAND, SurfaceBlocks.GRAVEL, null);
 
     public static SurfaceConfig getSurfaceConfig(Holder<Biome> biome, HolderLookup<SurfaceConfig> surfaceConfigLookup) {
         if (surfaceConfigLookup == null) {
@@ -33,18 +34,10 @@ public record SurfaceConfig(SurfaceBlocks normal, SurfaceBlocks beachSand, Surfa
         }
 
         Optional<Holder.Reference<SurfaceConfig>> optionalKey = surfaceConfigLookup./**/listElements()
-            .filter(entry -> entry.isBound() && biome.is(
-                TagKey.create(Registries.BIOME, VersionCompat.id(
-                    entry.key().location().getNamespace()
-                    + ":surface_config/"
-                    + entry.key().location().getPath()
-                ))))
+            .filter(entry -> entry.isBound() &&
+                    entry.value().biomeTag != null && biome.is(entry.value().biomeTag))
             .findFirst();
 
-        if (optionalKey.isPresent()) {
-            return optionalKey.get().value();
-        }
-
-        return DEFAULT;
+        return optionalKey.map(Holder.Reference::value).orElse(DEFAULT);
     }
 }

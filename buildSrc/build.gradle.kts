e6b6a1dedd5edb4e4f7cd68e0e97033bad418884
forkiesassds <icanttellyou@memeware.net>
2025-12-19 01:15:16 +0200

    build: Bump Stonecutter and Kotlin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.3.0"
 }
 
 repositories {
@@ -12,6 +12,6 @@ repositories {
 dependencies {
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
     //FIXME: This is hardcoded! Bad!
-    implementation("dev.kikugie:stonecutter:0.8-alpha.10")
+    implementation("dev.kikugie:stonecutter:0.8-beta.3")
     implementation(plugin("com.modrinth.minotaur", "2.+"))
 }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("dev.kikugie.stonecutter")
 
-    (kotlin("jvm") version "2.2.0").apply(false)
-    (id("com.google.devtools.ksp") version "2.2.0-2.0.2").apply(false)
+    (kotlin("jvm") version "2.3.0").apply(false)
+    (id("com.google.devtools.ksp") version "2.3.3").apply(false)
     (id("dev.kikugie.fletching-table") version "0.1.0-alpha.22").apply(false)
 }
 

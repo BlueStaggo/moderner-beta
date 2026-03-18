585659ea24b65b19bf23f732103b1ac4504f1b47
icanttellyou <cuboidedposition32@protonmail.com>
2026-03-18 10:31:26 +0200

    chore(fabric): Cleanup resource reload listener code

--- fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/ModernerBetaFabricClient.java
+++ fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/ModernerBetaFabricClient.java
@@ -5,6 +5,7 @@ import mod.bluestaggo.modernerbeta.client.ModernerBetaClient;
 import mod.bluestaggo.modernerbeta.client.color.block.BlockColorSampler;
 import mod.bluestaggo.modernerbeta.client.color.block.BlockColors;
 import mod.bluestaggo.modernerbeta.client.registry.ModernBetaClientRegistries;
+import mod.bluestaggo.modernerbeta.client.resource.ModernBetaColormapReloadListener;
 import mod.bluestaggo.modernerbeta.compat.client.ModCompatClient;
 import mod.bluestaggo.modernerbeta.fabric.registry.RegistryHelperImpl;
 import mod.bluestaggo.modernerbeta.network.BiomeProviderInfoPayload;
@@ -13,7 +14,9 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
 
 public class ModernerBetaFabricClient implements ClientModInitializer {
     @Override
@@ -27,32 +30,20 @@ public class ModernerBetaFabricClient implements ClientModInitializer {
 
         BlockColors.register(net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry.BLOCK::register);
 
-        //? if >=1.21.9 {
-        /*net.fabricmc.fabric.api.resource.v1.ResourceLoader resourceManager =
-                net.fabricmc.fabric.api.resource.v1.ResourceLoader.get(PackType.CLIENT_RESOURCES);
-        //~ if >=26.1 'registerReloader' -> 'registerReloadListener'
-        resourceManager.registerReloader(ModernerBeta.createId("water_colormap"), new mod.bluestaggo.modernerbeta.client.resource.ModernBetaColormapReloadListener(
-        *///? } else {
-        net.fabricmc.fabric.api.resource.ResourceManagerHelper resourceManager =
-                net.fabricmc.fabric.api.resource.ResourceManagerHelper.get(PackType.CLIENT_RESOURCES);
-        resourceManager.registerReloadListener(new mod.bluestaggo.modernerbeta.fabric.client.resource.ModernBetaFabricColormapReloadListener(
+        registerReloadListener(
             ModernerBeta.createId("water_colormap"),
-        //? }
-            "textures/colormap/water.png",
-            BlockColorSampler.INSTANCE.colormapWater::setColormap
-        ));
-
-        //? if >=1.21.9 {
-        /*//~ if >=26.1 'registerReloader' -> 'registerReloadListener'
-        resourceManager.registerReloader(ModernerBeta.createId("underwater_colormap"), new mod.bluestaggo.modernerbeta.client.resource.ModernBetaColormapReloadListener(
-        *///? } else {
-        resourceManager.registerReloadListener(new mod.bluestaggo.modernerbeta.fabric.client.resource.ModernBetaFabricColormapReloadListener(
+            new ModernBetaColormapReloadListener(
+                "textures/colormap/water.png",
+                BlockColorSampler.INSTANCE.colormapWater::setColormap
+            )
+        );
+        registerReloadListener(
             ModernerBeta.createId("underwater_colormap"),
-        //? }
-            "textures/colormap/underwater.png",
-            BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
-        ));
-
+            new ModernBetaColormapReloadListener(
+                "textures/colormap/underwater.png",
+                BlockColorSampler.INSTANCE.colormapUnderwater::setColormap
+            )
+        );
 
         //? if >=1.20.2 {
         ClientPlayNetworking.registerGlobalReceiver(BiomeProviderInfoPayload.ID, (payload, context) -> {
@@ -66,5 +57,37 @@ public class ModernerBetaFabricClient implements ClientModInitializer {
             minecraftClient.execute(() -> S2CPacketHandlers.onBiomeProviderInfo(minecraftClient.level, payload));
         });
         *///?}
+    }
+
+    private <T> void registerReloadListener(
+        ResourceLocation id,
+        SimplePreparableReloadListener<T> listener
+    ) {
+        //? if >=1.21.9 {
+        /*net.fabricmc.fabric.api.resource.v1.ResourceLoader resourceManager =
+                net.fabricmc.fabric.api.resource.v1.ResourceLoader.get(PackType.CLIENT_RESOURCES);
+        //~ if >=26.1 'registerReloader' -> 'registerReloadListener'
+        resourceManager.registerReloader(id, listener);
+        *///? } else {
+        net.fabricmc.fabric.api.resource.ResourceManagerHelper resourceManager =
+                net.fabricmc.fabric.api.resource.ResourceManagerHelper.get(PackType.CLIENT_RESOURCES);
+
+        resourceManager.registerReloadListener(new net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener() {
+            @Override
+            public ResourceLocation getFabricId() {
+                return id;
+            }
+
+            @Override
+            public java.util.concurrent.CompletableFuture<Void> reload(
+                PreparationBarrier barrier,
+                net.minecraft.server.packs.resources.ResourceManager manager,
+                java.util.concurrent.Executor backgroundExecutor,
+                java.util.concurrent.Executor gameExecutor
+            ) {
+                return listener.reload(barrier, manager, backgroundExecutor, gameExecutor);
+            }
+        });
+        //? }
     }
 }

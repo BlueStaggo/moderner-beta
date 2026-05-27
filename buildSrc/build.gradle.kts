f3bb3edcb9c924c8232b552320f82615e962fe43
icanttellyou <cuboidedposition32@protonmail.com>
2026-05-27 19:07:02 +0300

    publish: Update publishing plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
     //FIXME: This is hardcoded! Bad!
     implementation("dev.kikugie:stonecutter:0.9.4")
-    implementation(plugin("me.modmuss50.mod-publish-plugin", "1.1.0"))
+    implementation(plugin("me.modmuss50.mod-publish-plugin", "2.0.0-beta.2"))
 
     implementation("org.ow2.asm:asm:9.9")
     implementation("org.ow2.asm:asm-tree:9.9")
--- buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
@@ -42,6 +42,9 @@ publishMods {
         accessToken = System.getenv("CURSEFORGE_TOKEN")
         projectId = commonMod.prop("curseforge_project_id")
         minecraftVersions.addAll(commonMod.prop("supported_versions").split(",").toList())
+
+        client = true
+        server = true
     }
 
     github {

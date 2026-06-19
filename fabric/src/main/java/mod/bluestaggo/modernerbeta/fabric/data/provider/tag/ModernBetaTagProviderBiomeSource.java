21e99f2c4695026719b86116150a47d950106e4b
icanttellyou <cuboidedposition32@protonmail.com>
2026-06-19 17:48:43 +0300

    fix: Fixup from cherrypick

--- fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/provider/tag/ModernBetaTagProviderBiomeSource.java
+++ fabric/src/main/java/mod/bluestaggo/modernerbeta/fabric/data/provider/tag/ModernBetaTagProviderBiomeSource.java
@@ -2,8 +2,8 @@ package mod.bluestaggo.modernerbeta.fabric.data.provider.tag;
 
 import mod.bluestaggo.modernerbeta.ModernerBeta;
 import mod.bluestaggo.modernerbeta.util.VersionCompat;
-import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.core.registries.Registries;
@@ -13,8 +13,8 @@ import net.minecraft.world.level.biome.BiomeSource;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModernBetaTagProviderBiomeSource extends FabricTagsProvider<com.mojang.serialization.MapCodec<? extends BiomeSource>> {
-    public ModernBetaTagProviderBiomeSource(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registryLookupFuture) {
+public class ModernBetaTagProviderBiomeSource extends FabricTagProvider<com.mojang.serialization.MapCodec<? extends BiomeSource>> {
+    public ModernBetaTagProviderBiomeSource(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registryLookupFuture) {
         super(output, Registries.BIOME_SOURCE, registryLookupFuture);
     }
 

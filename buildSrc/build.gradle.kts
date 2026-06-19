9944ba221a17db92dfa135801b7f6800c17af14e
icanttellyou <cuboidedposition32@protonmail.com>
2026-06-19 16:58:00 +0300

    publish: Update publishing plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
     //FIXME: This is hardcoded! Bad!
     implementation("dev.kikugie:stonecutter:0.9.5")
-    implementation(plugin("me.modmuss50.mod-publish-plugin", "2.0.0-beta.2"))
+    implementation(plugin("me.modmuss50.mod-publish-plugin", "2.0.1"))
 
     implementation("org.ow2.asm:asm:9.9")
     implementation("org.ow2.asm:asm-tree:9.9")
